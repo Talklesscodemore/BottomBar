876f3718f2338b08f000bb3c33a39b43d78dcbbc
Roberto <talklesscodemore@gmail.com>
2016-06-13 09:47:38 +0200

    Fix lint contentDescription warnings
    Fixed contentDescription warnings

--- bottom-bar/src/main/java/com/roughike/bottombar/MiscUtils.java
+++ bottom-bar/src/main/java/com/roughike/bottombar/MiscUtils.java
@@ -20,7 +20,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
